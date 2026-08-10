b7c963d2e78b75e4c9af74b7d1469aa0ea76fb96
kopparthikomal-ops <kopparthikomal@gmail.com>
2026-08-10 12:21:40 +0530

    Create 69_Sqrt(x).java

---
+++ Problems/69_Sqrt(x).java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid; 
+            if (square == x) {
+                ans = mid;
+                break;
+            } else if (square < x) {
+                ans = mid;      
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
